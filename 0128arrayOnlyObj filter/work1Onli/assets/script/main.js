01f9e78ebbabbeacd4aa2919aaec23fc3161514e
Igor Gramovich <igorgramovich86@gmail.com>
2018-01-31 06:02:30 +0000

    правка уникального элемента без флага

--- 0128arrayOnlyObj filter/work1Onli/assets/script/main.js
+++ 0128arrayOnlyObj filter/work1Onli/assets/script/main.js
@@ -1,5 +1,5 @@
-var arr = [{g: 1}, {a: 2}, {a: 2}, {b: 2}, {c: 1, a: 5}, {d: 1}, {d: 1, b: 1}, {a: 1}, {b: 2}, {d: 1, b: 1}, {b: 2}];
-var arr1 = [{g: 1}, {a: 2}, {a: 2}, {a: 1, b: 2}];
+var arr = [{ g: 1 }, { a: 2 }, { a: 2 }, { b: 2 }, { c: 1, a: 5 }, { d: 1 }, { d: 1, b: 1 }, { a: 1 }, { b: 2 }, { d: 1, b: 1 }, { b: 2 }];
+var arr1 = [{ g: 1 }, { a: 2 }, { a: 2 }, { a: 1, b: 2 }];
 var arr2 = [];
 
 console.log(filterUniqueObject(arr));
@@ -8,31 +8,28 @@ console.log(arr);
 
 function filterUniqueObject(array) {
 
-	if ( !array.length ) {
-		return null;
-	}
 
-	var ansver = [array[0]];
-	var flag = true;
+	var ansver = [];
 
-	array.forEach(function (obj) {
-		for ( var i = 0; i < ansver.length; i++ ) {
-			if ( (checkedObject(obj, ansver[i])) ) {
-				flag = false;
+
+	array.forEach(function(obj) {
+		var flag = true;
+
+		for (var i = 0; i < ansver.length; i++) {
+			if ((checkedObject(obj, ansver[i]))) {
+				break;
 			}
 		}
-		if ( flag ) {
+		if (i === ansver.length) {
 			ansver.push(obj)
-		} else {
-			flag = true;
 		}
 	})
 
 	function checkedObject(obj1, obj2) {
 
-		if ( Object.keys(obj1).length === Object.keys(obj2).length ) {
-			for ( var key in obj1 ) {
-				if ( obj2[key] && obj1[key] === obj2[key] ) {
+		if (Object.keys(obj1).length === Object.keys(obj2).length) {
+			for (var key in obj1) {
+				if (obj2[key] && obj1[key] === obj2[key]) {
 					return true;
 				}
 			}
