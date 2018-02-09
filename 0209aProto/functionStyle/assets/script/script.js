7ec26b85cf85fd585579fd6a798f1aee78c9eacf
boltwest <boltwest@gmail.com>
2018-02-09 23:29:40 +0300

    First work

--- 0209aProto/functionStyle/assets/script/script.js
+++ 0209aProto/functionStyle/assets/script/script.js
@@ -1,49 +1,106 @@
-function Shape(arg){
-    
-    var name = arg;
-    
-    this.getName = function(){
-        return name;
-    }
+function Shape() {  //создали первый класс
+
+	var name = 'Фигура';
+
+	this.getName = function () {
+		return name;
+	}
 }
 
-function Rectangle(argA, argB){
-    Shape.apply(this, 'Фигура');
-    
-    var getName = this.getName;
-    this.getName = function(){
-        var name = getName();
-        return name + 'Прямоугольник'
-    }
-    
-    var a = argA;
-    var b = argB;
-    
-    this.setA = function (arg){
-        a = arg;
-    }
-    
-    this.setB = function(arg){
-        b = arg;
-    }
-    
-    this.getA = function(){
-        return a;
-    }
-    
-    this.getB = function(){
-        return b;
-    }
-    
-    this.getSquare = function(){
-        return a * b;
-    }
-    
-    this.setPerimeter = function(){
-        return (a + b) * 2;
-    }
+function Rectangle(argA, argB) { //создали второй класс
+	Shape.call(this);            //во втором вызвали первый, тем самым наследовали его
+
+	var getName = this.getName;         // записываем метод в переменную
+	this.getName = function () {        // перезаписывем новый метод
+		var name = getName();           // в новом методе вызываем старый
+		return name + ' прямоугольник'  // в новом добовляем действие
+	}
+
+	this._a = argA;
+	this._b = argB;
+
+	this.setA = function (arg) {
+		this._a = arg;
+	}
+
+	this.setB = function (arg) {
+		this._b = arg;
+	}
+
+	this.getA = function () {
+		return this._a;
+	}
+
+	this.getB = function () {
+		return this._b;
+	}
+
+	this.getSquare = function () {
+		return this._a * this._b;
+	}
+
+	this.getPerimeter = function () {
+		return (this._a + this._b) * 2;
+	}
 }
 
-function Square(argA){
-    
+function Square(argA) {      // создали третий класс
+	Rectangle.call(this);    // наследовали второй
+
+	this._a = argA;
+	this._b = argA;
+	var name = 'Фигура квадрат';
+
+	this.getName = function () {
+		return name;
+	}
+
+	var setA = this.setA;
+	this.setA = function (arg) {
+		setA.call(this, arg);
+		this._b = arg;
+	}
+
+	var setB = this.setB;
+	this.setB = function (arg) {
+		setB.call(this, arg);
+		this._a = arg;
+	}
 }
+
+
+var pr = new Rectangle(5, 6);
+console.log(pr.getName());
+
+
+var form = new Square(20);
+console.log(form.getName());
+console.log(form.getSquare());
+console.log(form.getPerimeter());
+form.setA(8);
+console.log(form.getA());
+console.log(form.getB());
+console.log(form.getSquare());
+console.log(form.getPerimeter());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
